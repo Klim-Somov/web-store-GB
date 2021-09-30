d63af93a2f003488f49115e9555169e309a6c967
Klim_somov <klim_somov@mail.ru>
2021-09-30 21:37:32 +0300

    chenge main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.photo__slider').slick({
         
-        prevArrow: '<button type="button" class="slick-prev"><img src="/img/slider-prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="/img/slider-next.svg" alt=""></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="./img/slider-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./img/slider-next.svg" alt=""></button>'
     });
     });
