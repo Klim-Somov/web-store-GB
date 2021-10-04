4608ebd385de53fa2b5dda63683ed890fddc422b
Klim_somov <klim_somov@mail.ru>
2021-10-04 16:17:08 +0300

    создал ветку, добавил гитигнор

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(function(){
     $('.photo__slider').slick({
         
-        prevArrow: '<button type="button" class="slick-prev"><img src="./img/slider-prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./img/slider-next.svg" alt=""></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img class="slider-svg" src="./img/slider-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img class="slider-svg" src="./img/slider-next.svg" alt=""></button>'
+      
+        
     });
     });
