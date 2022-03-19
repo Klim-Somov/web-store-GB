e9138630151da9379e9e5e8554b16eac674ff2a0
Klim <klim_somov@mail.ru>
2022-03-19 17:16:12 +0300

    imlement side menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-$(function(){
-    $('.photo__slider').slick({
-        
-        prevArrow: '<button type="button" class="slick-prev"></button>',
-        nextArrow: '<button type="button" class="slick-next"></button>'
-      
-        
-    });
-    });
+
+//    const iconMenu = document.querySelector('menu-icon');
+// if (iconMenu) {
+//     const headerMenu = document.querySelector('.header__menu');
+//     iconMenu.addEventListener("click", e => {
+//         headerMenu.classList.toggle('_active');
+//     });
+// }
+
+// const iconMenub = document.querySelector('.menu-icon');
+// if (iconMenu) {
+//     const icon = document.querySelector('.menu-icon');
+//     iconMenub.addEventListener("click", e => {
+//         icon.classList.toggle('_active-b');
+//     });
+// };
